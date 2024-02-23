8b71d8d092c70c2fbaeba70926d19c09faf8a096
Loop Shen <2947232977@qq.com>
2024-02-23 17:37:08 +0800

    交替落子

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
-
+// 任务一：排布
+// 任务二：数值可变
+// 任务三：单位操作
+// 任务四：交替落子
+// 任务五：阻止覆盖
 function App() {
 
   //定义状态
   // 状态一chessLayout:整个棋盘的布局（每个Box的落子情况列表）
   const [chessLayout, setChesslayout] = useState(Array(9).fill(0));
+  // 状态二chAnge：交替落子(+/-)
+  const [chAnge, setChange] = useState(true)
 
   //  组件一：棋盘格子 
   function Box({ value, onBoxClick }) {
@@ -16,9 +22,16 @@ function App() {
 
   function changeEvent(i) {
     const newchessLayout = chessLayout.slice()
-    newchessLayout[i] = 1
-    setChesslayout(newchessLayout)
-    console.log("click");
+    //根据当前chAnge状态 - 交替落子
+    if (newchessLayout[i] === 0) {
+      // 更新当前格子
+      chAnge ? newchessLayout[i] = '+' : newchessLayout[i] = '-'
+      // 更新棋局
+      setChesslayout(newchessLayout)
+      // 改变交替落子状态chAnge
+      setChange(!chAnge)
+      console.log("click");
+    }
   }
 
   return (
